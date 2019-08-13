7a96fcd7ab3b448cfbdaccac63b13b6b76ea3155
Lois Di Qual <lois@takescoop.com>
2019-08-13 13:26:48 -0700

    More frontend js fixes

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,4 @@
 const _ = require('lodash')
-const $ = require('jquery')
 const Joi = require('@hapi/joi')
 const ladda = require('ladda')
 
@@ -7,54 +6,80 @@ require('ladda/dist/ladda.min.css')
 
 const API_URL = window.location.hostname === 'buildnumber.dev' ? 'https://api.buildnumber.dev' : 'http://localhost:3000/api'
 
-$().ready(function() {
+const windowUrl = new URL(window.location.href)
+const token = windowUrl.searchParams.get('token') || 'API_TOKEN'
+document.querySelectorAll('.api-key').forEach(e => {
+    e.innerText = token
+})
 
-    const windowUrl = new URL(window.location.href)
-    const token = windowUrl.searchParams.get('token') || 'API_TOKEN'
-    $('.api-key').text(token)
+const emailField = document.querySelector('#email-field')
+const bundleIdentifierField = document.querySelector('#bundle-identifier-field')
+const signupButton = document.querySelector('#signup-button')
+const signupButtonLadda = ladda.create(signupButton)
+const form = document.querySelector('#signup-form')
+const resultContainer = document.querySelector('#signup-result')
+const errorContainer = document.querySelector('#signup-result .result-error')
+const successContainer = document.querySelector('#signup-result .result-success')
 
-    const $emailField = $('#email-field')
-    const $bundleIdentifierField = $('#bundle-identifier-field')
-    const signupButton = document.querySelector('#signup-button')
-    const signupButtonLadda = ladda.create(signupButton)
-    const $form = $('#signup-form')
-    const $errorContainer = $('#signup-result .result-error')
-    const $successContainer = $('#signup-result .result-success')
+function showError(html) {
+    resultContainer.classList.remove('closed')
+    errorContainer.innerHTML = html
+    errorContainer.classList.remove('hidden')
+    successContainer.classList.add('hidden')
+}
 
-    $form.submit(function(ev) {
-        ev.preventDefault()
-        const emailAddress = $emailField.val()
-        const bundleIdentifier = $bundleIdentifierField.val()
-        
-        try {
-            Joi.assert(emailAddress, Joi.string().email().required())
-        } catch (err) {
-            $errorContainer.text('Please enter a valid email address')
-            $errorContainer.slideDown(300)
-            return
+function showSuccess(html) {
+    resultContainer.classList.remove('closed')
+    errorContainer.classList.add('hidden')
+    successContainer.innerHTML = html
+    successContainer.classList.remove('hidden')
+}
+
+function hideResult() {
+    resultContainer.classList.add('closed')
+}
+
+form.addEventListener('submit', async ev => {
+    ev.preventDefault()
+    
+    const emailAddress = emailField.value
+    const bundleIdentifier = bundleIdentifierField.value
+    
+    try {
+        Joi.assert(emailAddress, Joi.string().email().required())
+    } catch (err) {
+        showError('Please enter a valid email address')
+        return
+    }
+
+    emailField.disabled = true
+    bundleIdentifierField.disabled = true
+    signupButtonLadda.start()
+    hideResult()
+    
+    fetch(`${API_URL}/tokens`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ emailAddress, bundleIdentifier })
+    })
+    .then(res => {
+        if (!res.ok) {
+            throw new Error(res.statusText)
         }
-
-        $emailField.prop('disabled', true)
-        signupButtonLadda.start()
-        $errorContainer.slideUp(300)
-        $successContainer.slideUp(300)
-
-        $.post({
-            url: API_URL + '/tokens',
-            contentType: 'application/json',
-            data: JSON.stringify({ emailAddress, bundleIdentifier })
-        }).success(function(data) {
-            const msg = `Your API token has been sent to ${emailAddress}<br>Check your emails!`
-            $successContainer.html(msg)
-            $successContainer.slideDown(300)
-        }).error(function(err) {
-            const errorText = _.get(err, 'responseJSON.error', 'Something went wrong, please try again!')
-            $errorContainer.text(errorText)
-            $errorContainer.slideDown(300)
-        }).always(function() {
-            $emailField.prop('disabled', false)
-            $bundleIdentifierField.prop('disabled', false)
-            signupButtonLadda.stop()
-        })
+    })
+    .then(() => {
+        const msg = `Your API token has been sent to ${emailAddress}<br>Check your emails!`
+        showSuccess(msg)
+    })
+    .catch(err => {
+        const errorText = _.get(err, 'responseJSON.error', 'Something went wrong, please try again!')
+        showError(errorText)
+    })
+    .finally(() => {
+        emailField.disabled = false
+        bundleIdentifierField.disabled = false
+        signupButtonLadda.stop()
     })
 })
